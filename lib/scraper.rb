f13bb394a46a40493763154c1e18481d75706668
HELEN HARMAN <hlharman@bellsouth.net>
2021-03-23 01:13:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,15 +2,11 @@ require 'pry'
 
 require 'nokogiri'
 require 'open-uri'
-
-
 require_relative './course.rb'
 
 class Scraper 
   
   def get_page
-    #html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses') 
-    #this holds the html code in this variable called html
      doc = Nokogiri::HTML(open('http://learn-co-curriculum.github.io/site-for-scraping/courses'))
     end 
   
